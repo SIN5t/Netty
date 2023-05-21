d45ca90d821936074efc2be1a31440005e6c48a0
SIN5t <97775060+SIN5t@users.noreply.github.com>
2023-05-21 20:25:53 +0800

    eco练习完成，还有很多要学习的地方！！

--- src/main/java/cn/edu/uestc/nettydemo/practice/Client.java
+++ src/main/java/cn/edu/uestc/nettydemo/practice/Client.java
@@ -1,37 +1,79 @@
 package cn.edu.uestc.nettydemo.practice;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.Scanner;
 
 @Slf4j
 public class Client {
     public static void main(String[] args) {
+        NioEventLoopGroup nioEventLoopGroup = new NioEventLoopGroup(2);
+
         ChannelFuture channelFuture = new Bootstrap()
-                .group(new NioEventLoopGroup(2))
-                .channel(NioServerSocketChannel.class)
+                .group(nioEventLoopGroup)
+                .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
                         log.info("init...");
                         nioSocketChannel.pipeline().addLast(new StringEncoder());
+                        nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf byteBuf = (ByteBuf) msg;
+                                log.info(byteBuf.toString(Charset.defaultCharset()));
+                            }
+                        });
                     }
                 })
                 .connect(new InetSocketAddress("localhost", 8080));
+
         log.info("channelFuture = {}",channelFuture.getClass().toString());
         //connect方法异步非阻塞
-        //本来没有单独拿到channelFuture的时候也要.channel，之后才能writeAndFlush
-        Channel channel = channelFuture.addListener((ChannelFutureListener) ChannelFuture::sync).channel();
+
+        //在另外一个线程而不是主线程中完成任务,(ChannelFutureListener)会在连接建立的时候被调用
+        channelFuture.addListener((ChannelFutureListener) future -> {
+            Channel channel = future.channel();
+            log.info("当前线程不是主线程，是：{}",Thread.currentThread().getName());
+
+            new Thread(() -> {
+                Scanner scanner = new Scanner(System.in);
+                while(true){
+                    String inputStr = scanner.nextLine();
+                    if ("q".equals(inputStr)) {
+                        //这时候应该关闭连接
+                        channel.close(); //这个是异步操作！
+                        //channel关闭后想要 优雅地关闭 nioEventLoopGroup,不能在这里，因为close是异步的方法！
+                        //解决方案：使用从channel中获取closeFuture对象
+                        break;
+                    }
+                    channel.writeAndFlush(inputStr);
+                    ChannelFuture closeFuture = channel.closeFuture();
+            /*log.debug("waiting close...");
+            closeFuture.sync();
+            log.debug("处理关闭之后的操作");*/
+                    closeFuture.addListener((ChannelFutureListener) channelFuture1 -> {
+                        log.info("当前线程是：{}",Thread.currentThread().getName());
+                        //这是lambda的写法，实际上new ChannelFutureListener()，重写方法
+                        nioEventLoopGroup.shutdownGracefully();
+                    });
+                }
+            }).start();
+
+        });
+
+
 
     }
-
 }
--- src/main/java/cn/edu/uestc/nettydemo/practice/Server.java
+++ src/main/java/cn/edu/uestc/nettydemo/practice/Server.java
@@ -6,14 +6,17 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
 
 /**
  * 写一个双向通信，客户端发什么，server就回复什么，eco
  */
 @Slf4j
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //1、启动类
         ChannelFuture channelFuture = new ServerBootstrap()
                 .group(new NioEventLoopGroup(1), new NioEventLoopGroup(2))
@@ -21,20 +24,31 @@ public class Server {
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        //nioSocketChannel.pipeline().addLast(new StringDecoder());// 将 ByteBuf 转换为字符串
                         nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 ByteBuf byteBuf = msg instanceof ByteBuf ? (ByteBuf) msg : null;
-                                if (byteBuf != null) {
+                               /* if (byteBuf != null) {
                                     byte[] bytes = new byte[16];
                                     byteBuf.readBytes(bytes, 0, byteBuf.readableBytes());
                                     log.debug(new String(bytes));
-                                }
+                                }*/
+                                log.info(byteBuf.toString(Charset.defaultCharset()));
+
+                                //ctx.writeAndFlush(msg);
+                                // 建议使用 ctx.alloc() 创建 ByteBuf
+                                ByteBuf response = ctx.alloc().buffer();
+                                response.writeBytes(byteBuf);
+                                ctx.writeAndFlush(response);
                             }
                         });
+
+
                     }
                 }).bind(8080);
-        channelFuture.addListener((ChannelFutureListener) ChannelFuture::sync);
+
+
 
     }
 
